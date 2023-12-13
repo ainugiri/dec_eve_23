ec1b615b7c17ee1c32b98d3d559bb8122f8e0bfa
ainugiri <giriprasa@gmail.com>
2023-12-13 19:29:33 +0530

    Added r2 files

---
+++ App.js
@@ -0,0 +1,38 @@
+import ExpenseItem from './ExpenseItem';
+import './App.css';
+
+function App() {
+  const expense = [
+    {
+      title:'phone',
+      amount:10000,
+      date:new Date(2023,11,9),
+    },
+    {
+      title:'laptop',
+      amount:12000,
+      date:new Date(2023,11,9),
+    },
+    {
+      title:'microphone',
+      amount:1000,
+      date:new Date(2023,11,9),
+    },
+    {
+      title:'refri',
+      amount:1300,
+      date:new Date(2023,11,9),
+    },
+  ]
+  return (
+    <div className="App">
+      <ExpenseItem title = {expense[0].title} amount ={expense[0].amount}  date = {expense[0].date} />
+      <ExpenseItem title = {expense[1].title} amount ={expense[1].amount}  date = {expense[1].date} />
+      <ExpenseItem title = {expense[2].title} amount ={expense[2].amount}  date = {expense[2].date} />
+      <ExpenseItem title = {expense[3].title} amount ={expense[3].amount}  date = {expense[3].date} />
+      
+    </div>
+  );
+}
+
+export default App;
