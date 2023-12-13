e0311b0e9acda75a7c6169736fbe7670d82d4e76
ainugiri <giriprasa@gmail.com>
2023-12-13 19:52:43 +0530

    added style to login

--- App.js
+++ App.js
@@ -14,11 +14,6 @@ function App() {
       date:new Date(2023,11,9),
     },
     {
-      title:'microphone',
-      amount:1000,
-      date:new Date(2023,11,9),
-    },
-    {
       title:'refri',
       amount:1300,
       date:new Date(2023,11,9),
@@ -26,7 +21,7 @@ function App() {
   ]
   return (
     <div className="App">
-      <ExpenseItem title = {expense[0].title} amount ={expense[0].amount}  date = {expense[0].date} />
+      <ExpenseItem title = {expense[0].title} amount ={expense[0].amount}  date = {expense[2].date} />
       <ExpenseItem title = {expense[1].title} amount ={expense[1].amount}  date = {expense[1].date} />
       <ExpenseItem title = {expense[2].title} amount ={expense[2].amount}  date = {expense[2].date} />
       <ExpenseItem title = {expense[3].title} amount ={expense[3].amount}  date = {expense[3].date} />
--- App.test.js
+++ App.test.js
@@ -5,4 +5,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});https://github.com/ainugiri/dec_eve_23.git
